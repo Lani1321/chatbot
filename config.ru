582b1e0b993a12c4949cddaeb5e67f14a19d7b1b
Lani1321 <lani.berry13@gmail.com>
2017-09-12 10:48:31 -0400

    send and receive texts from Twilio

--- config.ru
+++ config.ru
@@ -1,5 +1,7 @@
 # This file is used by Rack-based servers to start the application.
 
 require_relative 'config/environment'
-
+set :database, "postgres://localhost/[chatbot]"
 run Rails.application
+
+
--- db/migrate/20170830002219_create_users.rb
+++ db/migrate/20170830002219_create_users.rb
@@ -1,12 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.integer :id
       t.string :language
       t.string :phone
       t.integer :friend_id
-
-      t.timestamps
     end
   end
 end
