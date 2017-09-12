f16df91ed3c7bd9a00574dba070c3bb7d0560ec6
Lani1321 <lani.berry13@gmail.com>
2017-09-12 10:53:39 -0400

    fix routing syntax

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :receive_texts, :send_texts
-  get '/receive_texts/index' => '/incoming#index'
-  post '/receive_texts/index' => '/incoming#index'
+  get '/receive_texts/index' => 'receive_texts#index'
+  post '/receive_texts/index' => 'receive_texts#index'
 end
