a7c2e21baea75f90ae0805e1692bf4cca0f342ec
Lani1321 <lani.berry13@gmail.com>
2017-09-12 12:28:13 -0400

    add api so that @client recognizes account

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,17 @@
-class MessagesController < ActionController
-
+class MessagesController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+  
   def reply
     message_body = params["Body"]
     from_number = params["From"]
     twilio_phone_number = '2164782291'
-    boot_twilio
-    sms = @client.messages.create(
-      from: twilio_phone_number,
+    twilio_sid = 'ACf92fbc0df0473fba6673a485c2d3cf9c'
+    twilio_token = '1fbdc1437c16d0df71e8dd6c48beab33'
+    @client = Twilio::REST::Client.new twilio_sid, twilio_token
+    sms = @client.api.account.messages.create(
+      from: "+1#{twilio_phone_number}",
       to: from_number,
-      body: "Hello there, thanks for texting me. Your number is #{from_number}."
+      body: "Happy Tuesday!! Your number is #{from_number}."
     )
     
   end
--- config.ru
+++ config.ru
@@ -1,7 +1,6 @@
 # This file is used by Rack-based servers to start the application.
 
 require_relative 'config/environment'
-set :database, "postgres://localhost/[chatbot]"
 run Rails.application
 
 
