25a46966aa1b5ba8c00346147958d2885185b39d
Lani1321 <lani.berry13@gmail.com>
2017-09-14 14:59:00 -0400

    case statements for replies from Twilio

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,40 +5,95 @@ class MessagesController < ApplicationController
     message_body = params["Body"]
     from_number = params["From"]
     twilio_phone_number = '2164782291'
-    twilio_sid = 'ACf92fbc0df0473fba6673a485c2d3cf9c'
-    twilio_token = '1fbdc1437c16d0df71e8dd6c48beab33'
-    languages = EasyTranslate::LANGUAGES.values
-    # translation = EasyTranslate.translate("#{message_body}", :to => :th, :key => 'AIzaSyBukYm7kIRpauOVu6eH7oA-plDDWlEuQBg')
+    twilio_sid = ENV['TWILIO_ACCOUNT_SID']
+    twilio_token = ENV['TWILIO_AUTH_TOKEN']
+    languages = EasyTranslate::LANGUAGES.values.join("\n").upcase
+    # translation = EasyTranslate.translate("#{message_body}", :to => :th, :key => ENV['google_auth_key'])
     # languages = 
     #   EasyTranslate::LANGUAGES.values.each.with_index do |lang, i|
     #     puts "#{i+1}. #{lang}"
     #   end
+    # user = User.find_by_phone_number(from_number)
+    # if user != nil
     @client = Twilio::REST::Client.new twilio_sid, twilio_token
-    if message_body == 'Hello'
-      sms = @client.api.account.messages.create(
-        from: "+1#{twilio_phone_number}",
-        to: from_number,
-        body: "Welcome to Chatbot!! Your number is #{from_number}.  Here are a list of lanaguages I'm fluent in:#{languages}.  What language do you prefer to text in?"
-      )
-    elsif message_body == 'Thai' || 'Spanish' || 'Japanese'  || 'German' || 'Greek' || 'Turkish' || 'English' || 'Afrikaans' || 'Albanian' || 'Arabic' || 'Belarusian' || 'Chinese_simplified' || 'Croatian' || 'Czech' || 'Danish' || 'Dutch' || 'Estonian' || 'Filipino' || 'Finnish' || 'French' || 'Galician' || 'Hebrew' || 'Hindi' || 'Hungarian' || 'Icelandic' || 'Indonesian' || 'Irish' || 'Italian' || 'Japanese' || 'Korean' || 'Latin' || 'Latvian' || 'Lituanian' || 'Macedonian' || 'Malay' || 'Maltese' || 'Norwegian' || 'Persian' || 'Polish' || 'Poruguese' || 'Romanian' || 'Russian' || 'Serbian' || 'Slovak' || 'Slovenian' || 'Swahili' || 'Swedish' || 'Turkish' || 'Ukranian' || 'Vietnamese' || 'Welsh' || 'Yiddish'
-      sms = @client.api.account.messages.create(
-        from: "+1#{twilio_phone_number}",
-        to: from_number,
-        body: EasyTranslate.translate("Awesome, I speak #{message_body} too! It is really nice to meet you!", to: "#{message_body.downcase}", key: 'AIzaSyBukYm7kIRpauOVu6eH7oA-plDDWlEuQBg')
-      )
+    num_without_symbol = from_number.slice!(0..1)
+    new_num = from_number
+    user = User.find_by_phone_number(new_num)
+    if user != nil
+      case message_body
+      when /^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))?$/i
+        sms = @client.api.account.messages.create(
+          from: "+1#{twilio_phone_number}",
+          to: from_number,
+          body: "You want to text #{message_body}?"
+        )
+        user.friend_phone_number = "#{message_body}"
+        user.save
+      when "Thai", "Spanish", "Japanese" , "German", "Greek", "Turkish", "English", "Afrikaans", "Albanian", "Arabic", "Belarusian", "Chinese_simplified", "Croatian", "Czech", "Danish", "Dutch", "Estonian", "Filipino", "Finnish", "French", "Galician", "Hebrew", "Hindi", "Hungarian", "Icelandic", "Indonesian", "Irish", "Italian", "Japanese", "Korean", "Latin", "Latvian", "Lituanian", "Macedonian", "Malay", "Maltese", "Norwegian", "Persian", "Polish", "Poruguese", "Romanian", "Russian", "Serbian", "Slovak", "Slovenian", "Swahili", "Swedish", "Turkish", "Ukranian", "Vietnamese", "Welsh", "Yiddish"
+        sms = @client.api.account.messages.create(
+          from: "+1#{twilio_phone_number}",
+          to: from_number,
+          body: "You set your language as #{message_body}.  Please, enter the phone number you would like to message."
+        )
+        user.language = "#{message_body}"
+        user.save
+      when "Yes", "yes"
+        sms = @client.api.account.messages.create(
+          from: "+1#{twilio_phone_number}",
+          to: from_number,
+          body: "Alright, what would you like to say?"
+        )
+        self.send_sms
+      when "No", "no"
+        sms = @client.api.account.messages.create(
+          from: "+1#{twilio_phone_number}",
+          to: from_number,
+          body: "Alright, what phone number would you like to message?"
+        )
+        user.update(friend_phone_number: nil)
+      else
+        sms = @client.api.account.messages.create(
+          from: "+1#{twilio_phone_number}",
+          to: from_number,
+          body: "Welcome back! Please enter the phone number you would like to message"
+        )
+      end
+
     else
-      sms = @client.api.account.messages.create(
-        from: "+1#{twilio_phone_number}",
-        to: from_number,
-        body: "Welcome to Chatbot!! Your number is #{from_number}.  Here are a list of lanaguages I'm fluent in:#{languages}.  What language do you prefer to text in?"
-      )
+      case message_body 
+      when "Thai", "Spanish", "Japanese" , "German", "Greek", "Turkish", "English", "Afrikaans", "Albanian", "Arabic", "Belarusian", "Chinese_simplified", "Croatian", "Czech", "Danish", "Dutch", "Estonian", "Filipino", "Finnish", "French", "Galician", "Hebrew", "Hindi", "Hungarian", "Icelandic", "Indonesian", "Irish", "Italian", "Japanese", "Korean", "Latin", "Latvian", "Lituanian", "Macedonian", "Malay", "Maltese", "Norwegian", "Persian", "Polish", "Poruguese", "Romanian", "Russian", "Serbian", "Slovak", "Slovenian", "Swahili", "Swedish", "Turkish", "Ukranian", "Vietnamese", "Welsh", "Yiddish"
+        sms = @client.api.account.messages.create(
+          from: "+1#{twilio_phone_number}",
+          to: from_number,
+          body: "You set your language as #{message_body}.  Please, enter the phone number you would like to message."
+        )
+
+      else
+        user = User.create(
+          :phone_number => from_number
+        )
+        sms = @client.api.account.messages.create(
+          from: "+1#{twilio_phone_number}",
+          to: from_number,
+          body: "Welcome to Chatbot!! Here are a list of lanaguages I'm fluent in:\n#{languages}.\nWhat language do you prefer to text in?"
+        )
+      end
     end 
   end
 
   def send_sms
     to = params["To"]
     sms = params["Body"]
+    twilio_sid = ENV['TWILIO_ACCOUNT_SID']
+    twilio_token = ENV['TWILIO_AUTH_TOKEN']
+    twilio_phone_number = '2164782291'
+    user = User.new 
     @client = Twilio::REST::Client.new twilio_sid, twilio_token 
+    @client.api.account.messages.create(
+      to: to,
+      from: "+1#{twilio_phone_number}",
+      body: sms
+    )
   end
  
   private
