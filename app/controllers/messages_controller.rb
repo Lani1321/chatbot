9647cbe2de4f3c1c1ae4949a520042379bb72ab7
Lani1321 <lani.berry13@gmail.com>
2017-09-15 12:00:42 -0400

    filter out symbols for friend_phone numbers so there are no duplicate records in db

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,14 +5,15 @@ class MessagesController < ApplicationController
     message_body = params["Body"]
     from_number = params["From"]
     twilio_phone_number = '2164782291'
-    
+    twilio_sid = 'ACf92fbc0df0473fba6673a485c2d3cf9c'
+    twilio_token = '1fbdc1437c16d0df71e8dd6c48beab33'
     languages = EasyTranslate::LANGUAGES.values.join("\n").upcase
     # translation = EasyTranslate.translate("#{message_body}", :to => :th, :key => ENV[google_auth])
     # languages = 
     #   EasyTranslate::LANGUAGES.values.each.with_index do |lang, i|
     #     puts "#{i+1}. #{lang}"
     #   end
-    @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
+    @client = Twilio::REST::Client.new twilio_sid, twilio_token
     # Take out the first two characters to search in db bc Twilio adds +1 to params["from"]
     num_without_symbol = from_number.slice!(0..1)
     new_num = from_number
@@ -27,7 +28,9 @@ class MessagesController < ApplicationController
           to: from_number,
           body: "You want to text #{message_body}?"
         )
-        user.friend_phone_number = "#{message_body}"
+        friend_phone = "#{message_body}"
+        friend_phone_number_no_char = friend_phone.gsub(/[\-.()a-z\s]/,"")
+        user.friend_phone_number = friend_phone_number_no_char
         user.save
         # Create person 2 with given phone number if the record doesn't already exist
         if User.where(:phone_number => "#{user.friend_phone_number}").blank?
@@ -62,6 +65,7 @@ class MessagesController < ApplicationController
         # Look for person 2 in the database and check if language is nil =>prompt to set language
         friend = User.find_by_phone_number("#{user.friend_phone_number}")
         if friend.language == nil
+          #replace with twilio client
           @client.api.account.messages.create(
             from: "+1#{twilio_phone_number}",
             to: friend.phone_number, 
@@ -72,7 +76,7 @@ class MessagesController < ApplicationController
           @client.api.account.messages.create(
             from: "+1#{twilio_phone_number}",
             to: friend.phone_number,
-            body: EasyTranslate.translate("#{user.phone_number} says:\n#{message_body}", to: "#{friend.language.downcase}", key: ENV['google_auth'])
+            body: EasyTranslate.translate("#{user.phone_number} says:\n#{message_body}", to: "#{friend.language.downcase}", key: 'AIzaSyBukYm7kIRpauOVu6eH7oA-plDDWlEuQBg')
             # EasyTranslate.translate("#{user.phone_number} says:\n'#{message_body}", to: "#{friend.language.downcase}", key: 'AIzaSyBukYm7kIRpauOVu6eH7oA-plDDWlEuQBg')
             )
         end
@@ -82,36 +86,33 @@ class MessagesController < ApplicationController
       user = User.create(
         :phone_number => from_number
       )
-      @client.api.account.messages.create(
-        from: "+1#{twilio_phone_number}",
-        to: from_number,
-        body: "Welcome to Chatbot!! Here are a list of languages I'm fluent in:\n#{languages}.\nWhat language do you prefer to text in?"
-      )
+      send_sms(from_number, "Welcome to Chatbot!! Here are a list of languages I'm fluent in:\n#{languages}.\nWhat language do you prefer to text in?" )
+      # @client.api.account.messages.create(
+      #   from: "+1#{twilio_phone_number}",
+      #   to: from_number,
+      #   body: "Welcome to Chatbot!! Here are a list of languages I'm fluent in:\n#{languages}.\nWhat language do you prefer to text in?"
+      # )
       # end
     end 
   end
   # Create person 2 an account when user provides a friend phone number
   # have SMS take arguments, who you're sending it to, and what the message is)
   def send_sms(recipient_phone_number, message)
-    to = params["To"] #=> comment this line out if it takes arguments
-    sms = params["Body"] #=>coment this line out if it takes arguments
-    twilio_sid = ENV['TWILIO_ACCOUNT_SID'], 
-    twilio_token = ENV['TWILIO_AUTH_TOKEN']
+    # to = params["To"] #=> comment this line out if it takes arguments
+    # sms = params["Body"] #=>coment this line out if it takes arguments
     twilio_phone_number = '2164782291'
-    user = User.new 
-    @client = Twilio::REST::Client.new twilio_sid, twilio_token 
-    @client.api.account.messages.create(
-      to: to,
+    twilio_client.account.messages.create(
+      to: recipient_phone_number,
       from: "+1#{twilio_phone_number}",
-      body: sms
+      body: message
     )
   end
  
   private
  
-  def boot_twilio
+  def twilio_client
     twilio_sid = ENV['TWILIO_ACCOUNT_SID']
     twilio_token = ENV['TWILIO_AUTH_TOKEN']
-    @client = Twilio::REST::Client.new twilio_sid, twilio_token
+    Twilio::REST::Client.new twilio_sid, twilio_token
   end
 end
