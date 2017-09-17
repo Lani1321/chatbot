997db8022e82900bf6e42428309579affe72ecb2
Lani1321 <lani.berry13@gmail.com>
2017-09-17 15:24:19 -0400

    adding state to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
   validates_presence_of :phone_number
+
+  state_machine :state, :initial => :selecting_language do 
+    event :select_language do 
+      transition :selecting_language => :selecting_number
+    end
+  end
+
+  # This *must* be called, otherwise states won't get initialized
+  def initialize
+    super()
+  end
+
 end
