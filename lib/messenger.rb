b8336b47632b3b87b97950c693c967e2b2ae5a8c
Lani1321 <lani.berry13@gmail.com>
2017-09-18 17:10:59 -0400

    implement user state instead of having specific case statements

--- lib/messenger.rb
+++ lib/messenger.rb
@@ -5,19 +5,19 @@ class Messenger
     languages = EasyTranslate::LANGUAGES.values.join("\n").upcase
     # If user exists
     if user != nil
-      case message_body
-      
       # This filters phone numbers, country indicators, extensions, dashes, periods and parenthases
-      when /^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))?$/i
-        send_sms(from_number, "You want to text #{message_body}?")
+      # when /^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))?$/i
+      
+      if user.state == "selecting_number"  
+        send_sms(from_number, "You will be sending messages to: #{message_body}")
         
         # Filters white space and any non digit
         # Ensure that a new record doesn't get created for the same number
         # i.e. 330-957-7848 vs 3309577848 
         friend_phone = message_body
         friend_phone_number_no_char = friend_phone.gsub(/[\s\D]/,"")
-        user.friend_phone_number = friend_phone_number_no_char
-        user.save
+        user.update(friend_phone_number: friend_phone_number_no_char)
+        user.select_number
         
         # Create person 2 with given phone number if the record doesn't already exist
         if User.where(:phone_number => "#{user.friend_phone_number}").blank?
@@ -27,23 +27,19 @@ class Messenger
         end
       
       # Set language
-      when "Thai", "Spanish", "Japanese", "German", "Greek", "Turkish", "English", "Afrikaans", "Albanian", "Arabic", "Belarusian", "Chinese_simplified", "Croatian", "Czech", "Danish", "Dutch", "Estonian", "Filipino", "Finnish", "French", "Galician", "Hebrew", "Hindi", "Hungarian", "Icelandic", "Indonesian", "Irish", "Italian", "Japanese", "Korean", "Latin", "Latvian", "Lituanian", "Macedonian", "Malay", "Maltese", "Norwegian", "Persian", "Polish", "Poruguese", "Romanian", "Russian", "Serbian", "Slovak", "Slovenian", "Swahili", "Swedish", "Turkish", "Ukranian", "Vietnamese", "Welsh", "Yiddish"
+      # when "Thai", "Spanish", "Japanese", "German", "Greek", "Turkish", "English", "Afrikaans", "Albanian", "Arabic", "Belarusian", "Chinese_simplified", "Croatian", "Czech", "Danish", "Dutch", "Estonian", "Filipino", "Finnish", "French", "Galician", "Hebrew", "Hindi", "Hungarian", "Icelandic", "Indonesian", "Irish", "Italian", "Japanese", "Korean", "Latin", "Latvian", "Lituanian", "Macedonian", "Malay", "Maltese", "Norwegian", "Persian", "Polish", "Poruguese", "Romanian", "Russian", "Serbian", "Slovak", "Slovenian", "Swahili", "Swedish", "Turkish", "Ukranian", "Vietnamese", "Welsh", "Yiddish"
+      elsif user.state == "selecting_language"
         send_sms(from_number, "You set your language as #{message_body}.  Please, enter the phone number you would like to message.")
         user = User.find_by_phone_number(from_number)
         user.update(language: message_body)
-      
-      when "Yes", "yes"
-        send_sms(from_number, "Alright, what would you like to say?")
-      
-      when "No", "no"
-        send_sms(from_number, "Alright, what phone number would you like to message?")
-        user.update(friend_phone_number: nil)
-      
+        user.select_language
+
+      # User state will be messaging_friend
       else
         # Look for person 2 in the database and check if language is nil =>prompt to set language
         friend = User.find_by_phone_number("#{user.friend_phone_number}")
         if friend.language == nil
-          send_sms(friend_phone_number, "Hey there! #{user.phone_number} wants to chat with you. Here are a list of languages I'm fluent in:\n#{languages}.\nWhat language do you prefer to text in?")
+          send_sms(friend.phone_number, "Hey there! #{user.phone_number} wants to chat with you. Here are a list of languages I'm fluent in:\n#{languages}.\nWhat language do you prefer to text in?")
 
         # If person 2 has langauge set => send message
         else
@@ -52,6 +48,7 @@ class Messenger
       end
     
     # If user doesn't exist
+    # User state is selecting language
     else
       user = User.create(
         :phone_number => from_number
