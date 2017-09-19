042555ac0187815564338cf9b03a56c8640fb3f9
Lani1321 <lani.berry13@gmail.com>
2017-09-18 21:59:08 -0400

    add state for person two setting language

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,12 @@ class User < ApplicationRecord
   state_machine :state, :initial => :selecting_language do 
     event :select_language do 
       transition :selecting_language => :selecting_number
-      end
+    end
     event :select_number do
       transition :selecting_number => :messaging_friend
+    end
+    event :select_language_person_two do 
+      transition :selecting_language => :messaging_friend
     end
   end
 
